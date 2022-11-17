db37cf8eb0a8dd3eaa45122a50a73c08f193370a
DDDog-WANG <superalpacaaa@gmail.com>
2022-11-17 15:05:58 +0900

    Initial Commit

---
+++ EAT/EATApp.swift
@@ -0,0 +1,17 @@
+//
+//  EATApp.swift
+//  EAT
+//
+//  Created by 啊啊阿澄 on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct EATApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
